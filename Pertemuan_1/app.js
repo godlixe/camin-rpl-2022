e44844bd66985c132cc7d8e6b1b6c1e0361c1f13
godlixe <allzllx19@gmail.com>
2022-02-05 23:30:05 +0700

    done tasks 1, 2, and 3

--- Pertemuan_1/app.js
+++ Pertemuan_1/app.js
@@ -1,11 +1,38 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, res) => {
-    const url = req.url;
-    if(url === "/"){
-        res.write("<h1>Home page</h1>");
-        res.end();
+  if (req.method === "GET") {
+    if (req.url === "/") {
+      fs.readFile("./pages/index/index.html", (err, data) => {
+        res.setHeader("Content-Type", "text/html");
+        res.end(data);
+      });
+    } else if (req.url === "/contact") {
+      fs.readFile("./pages/contact/index.html", (err, data) => {
+        res.setHeader("Content-Type", "text/html");
+        res.end(data);
+      });
+    } else {
+      fs.readFile("./pages" + req.url, (err, data) => {
+        res.setHeader("Content-Type", "text/css");
+        console.log(req.method);
+        console.log(err);
+        res.end(data);
+      });
     }
-})
+  } else {
+    let inputData = "\n";
+    req.on("data", (data) => {
+      inputData += data;
+    });
+    req.on("end", () => {
+        fs.writeFile("contacts.txt", inputData, {flag: "a"}, (e) => {
+            console.log(e);
+        });
+    });
+  }
+});
 
-server.listen(5000)
+server.listen(5000);
