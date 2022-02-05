f038d882c1bbce9062227fabffe878e32a851b09
godlixe <allzllx19@gmail.com>
2022-02-05 15:57:44 +0700

    app js

--- Pertemuan_1/app.js
+++ Pertemuan_1/app.js
@@ -1,1 +1,11 @@
-console.log("hi");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    if(url === "/"){
+        res.write("<h1>Home page</h1>");
+        res.end();
+    }
+})
+
+server.listen(5000)
