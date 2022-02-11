2d3b69003dc77cce55940e7dfad736f32939f5a9
godlixe <allzllx19@gmail.com>
2022-02-11 17:02:38 +0700

    added 404 case and get contact list

--- Pertemuan_1/app.js
+++ Pertemuan_1/app.js
@@ -1,14 +1,19 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const {parse} = require('querystring');
 
 function sendResponseFile(url, req, res) {
   fs.readFile(url, (err, data) => {
-    res.setHeader(
-      "Content-Type",
-      !path.extname(req.url) ? "text/html" : "text/css"
-    );
-    res.end(data);
+    if (err) {
+      sendResponseFile("./pages/404/404.html", req, res);
+    } else {
+      res.setHeader(
+        "Content-Type",
+        !path.extname(req.url) ? "text/html" : "text/css"
+      );
+      res.end(data);
+    }
   });
 }
 
@@ -18,18 +23,20 @@ const server = http.createServer((req, res) => {
       sendResponseFile("./pages/index/index.html", req, res);
     } else if (req.url === "/contact") {
       sendResponseFile("./pages/contact/index.html", req, res);
+    }
+    else if(req.url === "/contactlist"){
+      sendResponseFile("contacts.txt", req, res);
     } else {
       sendResponseFile("./pages" + req.url, req, res);
     }
   } else {
     let inputData = "";
     req.on("data", (data) => {
-      inputData += data;
+      inputData += data.toString();
     });
     req.on("end", () => {
-      inputData += "\n";
-      fs.writeFile("contacts.txt", inputData, { flag: "a" }, (e) => {
-        if(e) console.log(e);
+      fs.writeFile("contacts.txt", JSON.stringify(parse(inputData), null, '\t') + '\n', { flag: "a" }, (e) => {
+        if (e) console.log(e);
       });
     });
     sendResponseFile("./pages/contact/index.html", req, res);
